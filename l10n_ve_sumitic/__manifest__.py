54279823997386624d343cdbc157e0ef4b48843a
EglimarTineo <eglimart22@gmail.com>
2026-04-07 00:12:03 -0400

    Update __manifest__.py

--- l10n_ve_sumitic/__manifest__.py
+++ l10n_ve_sumitic/__manifest__.py
@@ -13,21 +13,19 @@
         'base_vat',
         'l10n_latam_base',
         'l10n_latam_invoice_document',
-    ],
     'data': [
-        # Seguridad
         'security/ir.model.access.csv',
-        
-        # Datos Maestros (Carga de Estados, Municipios y Parroquias)
         'data/res_country_state_data.xml',
         'data/l10n_ve_hierarchy_data.xml',
-        
-        # Configuración Contable y Fiscal
         'data/account_tax_group_data.xml',
-        'data/account_tax_data.xml',
-        'data/ir_cron_data.xml',
         
-        # Vistas de Usuario (Interfaz)
+        # 1. Cargamos las cuentas contables primero
+        'data/account_account_data.xml', 
+        
+        # 2. Luego cargamos los impuestos (que usan esas cuentas)
+        'data/account_tax_data.xml',
+        
+        'data/ir_cron_data.xml',
         'views/res_partner_views.xml',
         'views/res_company_views.xml',
         'views/res_config_settings_views.xml',

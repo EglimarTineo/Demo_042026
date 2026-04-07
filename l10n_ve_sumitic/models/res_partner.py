c3ab9b18ee82cbbfd613be873ff1c9a1e2209f90
EglimarTineo <eglimart22@gmail.com>
2026-04-06 23:33:10 -0400

    Create res_partner.py

---
+++ l10n_ve_sumitic/models/res_partner.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    # Nuevos campos para la localización Sumitic
+    municipality_id = fields.Many2one('res.country.state.municipality', string='Municipio')
+    parish_id = fields.Many2one('res.country.state.parish', string='Parroquia')
+
+class ResCountryState(models.Model):
+    _inherit = 'res.country.state'
+
+    municipality_ids = fields.One2many('res.country.state.municipality', 'state_id', string='Municipios')
+
+class ResCountryStateMunicipality(models.Model):
+    _name = 'res.country.state.municipality'
+    _description = 'Municipios de Venezuela'
+    _order = 'name'
+
+    name = fields.Char(string='Municipio', required=True)
+    state_id = fields.Many2one('res.country.state', string='Estado', required=True)
+    parish_ids = fields.One2many('res.country.state.parish', 'municipality_id', string='Parroquias')
+
+class ResCountryStateParish(models.Model):
+    _name = 'res.country.state.parish'
+    _description = 'Parroquias de Venezuela'
+    _order = 'name'
+
+    name = fields.Char(string='Parroquia', required=True)
+    municipality_id = fields.Many2one('res.country.state.municipality', string='Municipio', required=True)

0aa8d20c9122931dbe2cd150f87eb1d14acf96e6
EglimarTineo <eglimart22@gmail.com>
2026-04-07 00:06:54 -0400

    Update __manifest__.py

--- l10n_ve_sumitic/__manifest__.py
+++ l10n_ve_sumitic/__manifest__.py
@@ -1,35 +1,37 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Venezuela - Contabilidad Sumitic',
+    'name': 'Localización Venezolana Sumitic 360',
     'version': '19.0.1.0.0',
-    'summary': 'Localización Contable para Venezuela adaptada para Odoo 19 Enterprise',
-    'category': 'Accounting/Localizations/Account Charts',
-    'author': 'Sumitic',
+    'category': 'Accounting/Localizations',
+    'summary': 'Adaptación de Odoo 19 para la normativa fiscal venezolana (SENIAT)',
+    'author': 'Sumitic 2205 C.A.',
     'website': 'https://sumitic.lat',
     'license': 'LGPL-3',
     'depends': [
         'base',
         'account',
-        'account_accountant',
-        'account_asset',
         'base_vat',
-        'l10n_ve',
+        'l10n_latam_base',
+        'l10n_latam_invoice_document',
     ],
     'data': [
+        # Seguridad
         'security/ir.model.access.csv',
+        
+        # Datos Maestros (Carga de Estados, Municipios y Parroquias)
         'data/res_country_state_data.xml',
         'data/l10n_ve_hierarchy_data.xml',
+        
+        # Configuración Contable y Fiscal
         'data/account_tax_group_data.xml',
-        
-        # --- CAMBIO CRÍTICO AQUÍ ---
-        'data/account_account_data.xml', # 1. Primero creamos las cuentas
-        'data/account_tax_data.xml',     # 2. Luego los impuestos que usan esas cuentas
-        # ---------------------------
-        
+        'data/account_tax_data.xml',
         'data/ir_cron_data.xml',
+        
+        # Vistas de Usuario (Interfaz)
         'views/res_partner_views.xml',
         'views/res_company_views.xml',
         'views/res_config_settings_views.xml',
-        'views/account_move.xml',
+        'views/account_move_views.xml',
     ],
     'installable': True,
     'application': True,

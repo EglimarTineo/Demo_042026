193dcfea8ce3aedfa44ee75d92d7e48eef48e5ac
EglimarTineo <eglimart22@gmail.com>
2026-04-06 22:42:05 -0400

    Update __manifest__.py

--- l10n_ve_sumitic/__manifest__.py
+++ l10n_ve_sumitic/__manifest__.py
@@ -2,22 +2,24 @@
     'name': 'Venezuela - Contabilidad Sumitic',
     'version': '19.0.1.0.0',
     'summary': 'Localización Contable para Venezuela adaptada para Odoo 19',
-    'category': 'Accounting/Localizations/Account Charts', # <--- CORREGIDO CON 'S'
+    'category': 'Accounting/Localizations/Account Charts',
     'author': 'Sumitic',
     'website': 'https://sumitic.lat',
     'license': 'LGPL-3',
     'depends': [
         'account',
-        'account_accountant', # RECOMENDADO: Para que active el menú de Contabilidad completo
+        'account_accountant', 
+        'account_asset',      # <--- AGREGAR ESTO: Evita el error de 'create_asset'
         'base_vat',
         'l10n_ve',
     ],
     'data': [
         'security/ir.model.access.csv',
         'data/res_country_state_data.xml',
-        'data/account_account_data.xml',
+        # 'data/account_account_data.xml', # Recomendación: Ponerlo después de impuestos si estos se usan en las cuentas
         'data/account_tax_group_data.xml',
         'data/account_tax_data.xml',
+        'data/account_account_data.xml', # <--- MOVIDO: Mejor cargar cuentas después de grupos e impuestos
         'data/ir_cron_data.xml',
         'views/res_company_views.xml',
         'views/res_config_settings_views.xml',

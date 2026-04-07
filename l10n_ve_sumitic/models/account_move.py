b5176dcab63273fed32b5e98b43ca54997821501
EglimarTineo <eglimart22@gmail.com>
2026-04-06 21:33:30 -0400

    Update account_move.py

--- l10n_ve_sumitic/models/account_move.py
+++ l10n_ve_sumitic/models/account_move.py
@@ -4,9 +4,17 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # Campo para el número de control (requerido en Venezuela)
+    l10n_ve_control_number = fields.Char(
+        string='Número de Control',
+        copy=False,
+        help='Número de control del documento (formato 00-000000)'
+    )
+
+    # Definimos el tipo de contribuyente de forma independiente
     l10n_ve_taxpayer_type = fields.Selection([
         ('res_re', 'Persona Natural Residente'),
         ('res_nr', 'Persona Natural No Residente'),
         ('dom_re', 'Persona Jurídica Domiciliada'),
         ('dom_nr', 'Persona Jurídica No Domiciliada'),
-    ], string='Tipo de Contribuyente', help='Tipo de contribuyente para la localización venezolana')
+    ], string='Tipo de Contribuyente')

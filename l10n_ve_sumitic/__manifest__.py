2be31e583a92e6e1a92bf2d161a846dce25ed257
EglimarTineo <eglimart22@gmail.com>
2026-04-07 00:57:12 -0400

    Update __manifest__.py

--- l10n_ve_sumitic/__manifest__.py
+++ l10n_ve_sumitic/__manifest__.py
@@ -51,7 +51,7 @@
         'views/res_partner_views.xml',
         'views/res_company_views.xml',
         'views/res_config_settings_views.xml',
-        'views/account_move_views.xml',
+        'views/account_move.xml',
     ],
     
     'installable': True,
